367caf1797393f5ef2fdf0bc7d9f815dbae5950c
Anthony Ameh <antonineutron@gmail.com>
2024-09-11 09:16:46 +0100

    made tweaks

--- web/components/view_blink.tsx
+++ web/components/view_blink.tsx
@@ -1,9 +1,11 @@
 "use client"
-import '@dialectlabs/blinks/index.css';
+// import '@dialectlabs/blinks/index.css';
 import { useState, useEffect } from 'react';
 import { Action, Blink, ActionsRegistry, useAction } from "@dialectlabs/blinks";
-import { useActionSolanaWalletAdapter } from "@dialectlabs/blinks/dist/hooks/solana";
+// @ts-ignore
+import { useActionSolanaWalletAdapter } from "@dialectlabs/blinks/hooks/solana";
 import { clusterApiUrl } from '@solana/web3.js';
+import { WalletAdapterNetwork } from '@solana/wallet-adapter-base';
 
 // needs to be wrapped with <WalletProvider /> and <WalletModalProvider />
 const UnfurlBlink = () => {
@@ -11,7 +13,8 @@ const UnfurlBlink = () => {
     const actionApiUrl = 'https://setita.com/blink/create';
     
     // useAction initiates registry, adapter and fetches the action.
-    const { adapter } = useActionSolanaWalletAdapter(clusterApiUrl('mainnet-beta'));
+    const { adapter } = useActionSolanaWalletAdapter(clusterApiUrl(WalletAdapterNetwork.Mainnet));
+    // const adapter = new UnsafeBurnerWalletAdapter();
     const { action } = useAction({ url: actionApiUrl, adapter });
 
     return action ? <Blink action={action} websiteText={new URL(actionApiUrl).hostname} /> : null;
