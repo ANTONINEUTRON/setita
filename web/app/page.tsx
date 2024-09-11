4f494372a0a9cb11fde358dd2bd6dfa4dea2b988
Anthony Ameh <antonineutron@gmail.com>
2024-09-11 20:37:06 +0100

    fix some issues

--- web/app/page.tsx
+++ web/app/page.tsx
@@ -18,13 +18,13 @@ export default function Page() {
         </header>
 
         {/* Hero Section */}
-        <section className="relative flex-1 flex flex-col justify-center items-center text-center bg-gradient-to-b from-primary to-secondary text-white py-20">
-          <h1 className="text-4xl font-bold mb-4">Crowdfunding with Accountability</h1>
+        <section className="relative flex-1 flex flex-col justify-center items-center text-center bg-gradient-to-b from-primary to-secondary text-white py-20 px-2 md:px-0">
+          <h1 className="text-4xl font-bold mb-4">Fundraising with Accountability</h1>
           <p className="max-w-xl mb-6">
             Milestone-Based Crowdfunding on Web3: Empowering Fundraisers to Build Donor Trust
           </p>
-          <Link href="/blink/view/">
-          {/* <Link href="https://dial.to/?action=solana-action%3Ahttps://setita.com/blink/create"> */}
+          {/* <Link href="/blink/view/">/ */}
+          <Link href="https://dial.to/?action=solana-action%3Ahttps://setita.com/blink/create">
             <button className="px-6 py-2 bg-gradient-to-tr from-primary to-secondary text-white rounded hover:bg-primary hover:shadow-2xl">
               Try out our Blink
             </button>
@@ -33,8 +33,22 @@ export default function Page() {
         </section>
         
       </div>
+      <section className='bg-secondary opacity-90 text-white flex flex-col items-center p-8'>
+        <div className='font-bold text-2xl my-8'>HOW IT WORKS</div>
+        <div className='grid grid-cols-1 md:grid-cols-3 text-black'>
+          <HowItWorksItem
+            titleNum='1'
+            details='Fill in details of your fundraising campaign' />
+          <HowItWorksItem
+            titleNum='2'
+            details='Create Campaign' />
+          <HowItWorksItem
+            titleNum='3'
+            details='Share the blinks with your donor' />
+        </div>
+      </section>
       {/* About us */}
-      <div className='flex flex-col justify-center items-center py-10 text-lg'>
+      <div className='flex flex-col justify-center items-center py-10 text-lg text-center px-2 md:px-0'>
         <div className='font-semibold text-2xl'>We are currently building and integrating other features</div>
         <div>Help us in achieving this lofty goal by Joining our waitlist</div>
         <div>
@@ -59,3 +73,18 @@ function GetStartedButton() {
   );
 }
 
+function HowItWorksItem({titleNum, details}:{
+  titleNum: string,
+  details: string,
+}){
+  return (
+    <div className='flex flex-col items-center bg-white m-5 rounded-lg p-6'>
+      <div className='bg-gradient-to-tr from-primary to-secondary text-white rounded-full px-2'>
+        {titleNum}
+      </div>
+      <span className='mt-4 text-lg text-center'>
+        {details}
+      </span>
+    </div>
+  )
+}
