4dc09c5a8bfee73d10b8ef63a2bb913c7fd99115
Anthony Ameh <antonineutron@gmail.com>
2024-11-27 13:36:15 +0100

    fix

--- web/app/react-query-provider.tsx
+++ web/app/react-query-provider.tsx
@@ -3,16 +3,13 @@
 import React, { ReactNode, useState } from 'react';
 import { ReactQueryStreamedHydration } from '@tanstack/react-query-next-experimental';
 import { QueryClientProvider, QueryClient } from '@tanstack/react-query';
-import { BuildType, OktoProvider } from 'okto-sdk-react';
 
 export function ReactQueryProvider({ children }: { children: ReactNode }) {
   const [client] = useState(new QueryClient());
 
   return (
     <QueryClientProvider client={client}>
-      <OktoProvider apiKey={process.env.OKTO_API_KEY ?? ""} buildType={BuildType.SANDBOX}>
         <ReactQueryStreamedHydration>{children}</ReactQueryStreamedHydration>
-      </OktoProvider>
     </QueryClientProvider>
   );
 }
